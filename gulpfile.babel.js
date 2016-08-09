835a220226fea59846a3b38e79de2200cb1243f1
Joe Sweeney <joe@j1.io>
2016-08-08 21:59:57 -0400

    Gulp Nodemon integration

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,42 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import babel from 'gulp-babel';
+import nodemon from 'gulp-nodemon';
+import Cache from 'gulp-file-cache';
+
+let cache = new Cache();
 
 gulp.task('sass', () => {
-  gulp.src('./lib/sass/**/*.scss')
+  let stream = gulp.src('./lib/sass/**/*.scss')
+    .pipe(cache.filter())
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(cache.cache())
     .pipe(gulp.dest('./public/css/'));
+
+  return stream;
 });
 
 gulp.task('babel', () => {
-  gulp.src('./lib/index.js')
+  let stream = gulp.src('./lib/index.js')
+    .pipe(cache.filter())
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(cache.cache())
+    .pipe(gulp.dest('./dist'));
+
+  return stream;
 });
 
-gulp.task('default', () => {
+gulp.task('default', ['babel', 'sass'], function () {
+  let stream = nodemon({
+    script: './dist/index.js',
+    watch: './lib',
+    ext: 'js hbs',
+    tasks: ['babel']
+    });
+
   gulp.watch('./lib/sass/**/*.scss', ['sass']);
-  gulp.watch('./lib/**/*.js', ['babel']);
+
+  return stream;
 });
