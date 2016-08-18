6fe3711584754db3ccf27a46e82f105ad9acbb61
Joe Sweeney <joe@j1.io>
2016-08-18 10:48:30 -0400

    Change cache packages

--- config/config.js
+++
@@ -1,7 +0,0 @@
-var clientId = 'd3d21a190f7a45bebc6d4df68b648c36'
-var clientSecret = '7870def1eac1495cb78fdf896deecb03'
-
-module.exports = {
-	clientId: clientId,
-	clientSecret: clientSecret
-}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import nodemon from 'gulp-nodemon';
-import gulpCache from 'gulp-file-cache';
+import cache from 'gulp-cached';
 import browserSync from 'browser-sync';
 import sourcemaps from 'gulp-sourcemaps';
 import jshint from 'gulp-jshint';
@@ -9,26 +9,25 @@ import stylish from 'jshint-stylish';
 import webpack from 'webpack-stream';
 import webpackConfig from './config/webpack.config.js';
 
-let cache = new gulpCache();
-
 gulp.task('webpack', () => {
   return gulp.src('./lib/client/js/pages')
+    .pipe(cache('webpack'))
     .pipe(webpack(webpackConfig.dev))
     .pipe(gulp.dest('./dist/client/webpack'));
 });
 
 gulp.task('babel', () => {
-  return gulp.src('./lib/server/index.js')
-    .pipe(cache.filter())
+  return gulp.src('./lib/server/**/*.js')
+    .pipe(cache('babel'))
     .pipe(babel({
-      presets: ['es2015']
+      presets: ['es2015', 'stage-2']
     }))
-    .pipe(cache.cache())
     .pipe(gulp.dest('./dist/server'));
 });
 
 gulp.task('jshint', () => {
   return gulp.src(['./lib/**/*.js', '!./lib/**/*.min.js'])
+    .pipe(cache('jshint'))
     .pipe(jshint({ esversion: 6 }))
     .pipe(jshint.reporter(stylish));
 });
@@ -72,10 +71,14 @@ gulp.task('watch', ['browser-sync'], () => {
   gulp.watch('./views/**/*.hbs', ['bs-reload']);
 });
 
+gulp.task('watch2', ['webpack'], () => {
+  gulp.watch('./lib/**/*.js', ['webpack']);
+});
+
 gulp.task('build', () => {
   gulp.src('./lib/server/index.js')
     .pipe(babel({
-      presets: ['es2015']
+      presets: ['es2015', 'stage-2']
     }))
     .pipe(gulp.dest('./dist/server'));
 
