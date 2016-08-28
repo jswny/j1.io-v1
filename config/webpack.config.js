2f46e19cd1e80deca8fb41217dc5171a87e6bad8
Joe Sweeney <joe@j1.io>
2016-08-28 13:07:33 -0400

    Simplify Webpack config, begin styling blog

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,70 +1,41 @@
 import commons from 'webpack/lib/optimize/CommonsChunkPlugin';
 
-export default {
-  dev: {
-    entry: {
-      home: './lib/client/js/pages/home.js',
-      projects: './lib/client/js/pages/projects.js'
-    },
-    output: {
-      filename: '[name].js',
-      publicPath: '/webpack/'
-    },
-    module: {
-      loaders: [
-        { test: /\.css$/, loader: 'style!css' },
-        { test: /\.scss$/, loaders: ['style', 'css?sourceMap', 'sass'] },
-        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel',
-          query: {
-            presets: ['es2015', 'stage-2']
-          }
+let master = {
+  entry: {
+    home: './lib/client/js/pages/home.js',
+    projects: './lib/client/js/pages/projects.js',
+    post: './lib/client/js/pages/post.js'
+  },
+  output: {
+    filename: '[name].js',
+    publicPath: '/webpack/'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loaders: ['style', 'css?sourceMap', 'sass'] },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-2']
         }
-      ]
-    },
-    sassLoader: {
-      sourceMap: true,
-      outputStyle: 'compressed'
-    },
-    plugins: [
-      new commons('commons.js', ['home', 'projects']),
+      }
     ]
   },
-  prod: {
-    entry: {
-      home: './lib/client/js/pages/home.js',
-      projects: './lib/client/js/pages/projects.js'
-    },
-    output: {
-      filename: '[name].js',
-      publicPath: '/webpack/'
-    },
-    module: {
-      loaders: [
-        { test: /\.css$/, loader: 'style!css' },
-        { test: /\.scss$/, loaders: ['style', 'css?sourceMap', 'sass'] },
-        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel',
-          query: {
-            presets: ['es2015', 'stage-2']
-          }
-        }
-      ]
-    },
-    sassLoader: {
-      sourceMap: true,
-      outputStyle: 'compressed'
-    },
-    plugins: [
-      new commons('commons.js', ['home', 'projects'])
-    ]
-  }
+  sassLoader: {
+    sourceMap: true,
+    outputStyle: 'compressed'
+  },
+  plugins: [
+    new commons('commons.js', ['home', 'projects', 'post'])
+  ]
+}
+
+export default {
+  dev: master,
+  prod: master
 }
