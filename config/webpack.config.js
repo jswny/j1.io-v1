9b9b70a5d2ae22a27f591f05f4f2ddf8127929ee
Joe Sweeney <joe@j1.io>
2016-08-30 03:33:28 -0400

    Optimize Webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,6 @@
 import commons from 'webpack/lib/optimize/CommonsChunkPlugin';
 
-let master = {
+let dev = {
   entry: {
     home: './lib/client/js/pages/home.js',
     projects: './lib/client/js/pages/projects.js',
@@ -35,7 +35,42 @@ let master = {
   ]
 }
 
+let prod = {
+  entry: {
+    home: './lib/client/js/pages/home.js',
+    projects: './lib/client/js/pages/projects.js',
+    post: './lib/client/js/pages/post.js'
+  },
+  output: {
+    filename: '[name].js',
+    publicPath: '/webpack/'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css?minimize' },
+      { test: /\.scss$/, loaders: ['style', 'css?minimize', 'sass'] },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-2']
+        }
+      }
+    ]
+  },
+  sassLoader: {
+    sourceMap: true,
+    outputStyle: 'compressed'
+  },
+  plugins: [
+    new commons('commons.js', ['home', 'projects', 'post'])
+  ]
+}
+
 export default {
-  dev: master,
-  prod: master
+  dev: dev,
+  prod: prod
 }
