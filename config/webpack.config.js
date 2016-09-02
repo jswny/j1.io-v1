60ce2724e398cb12936acda6b34127ce30707595
Joe Sweeney <joe@j1.io>
2016-09-02 02:36:57 -0400

    Improve Webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,12 +10,12 @@ let dev = {
     filename: '[name].js',
     publicPath: '/webpack/'
   },
+  devtool: 'source-map',
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css?sourceMap' },
       { test: /\.scss$/, loaders: ['style', 'css?sourceMap', 'sass'] },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      { test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url' },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
@@ -27,8 +27,7 @@ let dev = {
     ]
   },
   sassLoader: {
-    sourceMap: true,
-    outputStyle: 'compressed'
+    sourceMap: true
   },
   plugins: [
     new commons('commons.js', ['home', 'projects', 'post'])
@@ -49,8 +48,8 @@ let prod = {
     loaders: [
       { test: /\.css$/, loader: 'style!css?minimize' },
       { test: /\.scss$/, loaders: ['style', 'css?minimize', 'sass'] },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file' },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
